70686901b10795e3323ec6f823dd752f4fc75d95
Rohil Tuli <rohil.tuli@gmail.com>
2021-05-02 15:33:55 -0400

    feat: simple parser working
    the UI has a grid of cards for each journal entry, presented after a file is uploaded

--- src/App.svelte
+++ src/App.svelte
@@ -31,14 +31,25 @@
 		{#await files[0].text()}
 			<p>Loading...</p>
 		{:then fileContents}
-			<p>The text is {fileContents}</p>
+			<div class="flex flex-wrap gap-4 justify-items-center">
+				{#each parseFileContents(fileContents) as journalEntry}
+					<div class="bg-gray-100 bg-opacity-50 rounded-xl p-8 ring ring-gray-100 shadow">
+						<div class="text-indigo-600 text-center font-italic">{journalEntry.date.toDateString()}</div>
+						<br/>
+						<div class="text-2xl text-uppercase font-bold">{journalEntry.activity}</div>
+						<ul class="list-disc list-inside">
+							{#each journalEntry.properties as prop}
+								<li>{prop}</li>
+							{/each}
+						</ul>
+					</div>
+				{/each}
+			</div>
 		{/await}
 	{:else}
 		<p>Upload a file to get started.</p>
 		<input type="file" bind:files>
 	{/if}
-	<!-- <h1>Hello there!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
 </main>
 
 <style global lang="postcss">
