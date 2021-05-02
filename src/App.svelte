505dcff2b0b2f5af1a184a18abf8e3954f16b07a
Rohil Tuli <rohil.tuli@gmail.com>
2021-05-02 15:22:45 -0400

    feat: simple file upload working

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,44 @@
 <script lang="ts">
-	export let name: string;
+    interface JournalEntry {
+      date: Date,
+      activity: String,
+      properties: String[],
+    }
+
+    let files: FileList;
+
+    const parseJournalEntry = (contents: String) => {
+      let vals = contents.split(/\r?\n/);
+      return {
+        ...parseDateActivity(vals[0]),
+        properties: vals.slice(1)
+      }      
+    }
+
+    const parseDateActivity = (contents: String) => {
+      let vals = contents.split(/ /);
+      return {
+        date: new Date(Date.parse(vals[0])),
+        activity: vals[1],
+      }
+    }
+
+    const parseFileContents = (fileContents: String): JournalEntry[] => fileContents.split(/\r?\n\r?\n/).map(parseJournalEntry)
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#if files}
+		{#await files[0].text()}
+			<p>Loading...</p>
+		{:then fileContents}
+			<p>The text is {fileContents}</p>
+		{/await}
+	{:else}
+		<p>Upload a file to get started.</p>
+		<input type="file" bind:files>
+	{/if}
+	<!-- <h1>Hello there!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
 </main>
 
 <style global lang="postcss">
