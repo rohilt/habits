6620a57d3ba828673aec322663c02a0b8f3e66ca
Rohil Tuli <rohil.tuli@gmail.com>
2021-05-04 17:44:55 -0400

    test: added in parseHeader tests

--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -1,7 +1,21 @@
-import { parseFileContents } from '../parser/parser';
+import { parseFileContents, parseHeader } from '../parser/parser';
 
-const add = (x: number, y: number): number => x + y;
-
-test('should work', () => {
-	expect(add(2, 2)).toEqual(4);
+it('parseHeader: simple', () => {
+	expect(parseHeader('2020-01-01 ACTIVITY')).toEqual({
+		parseType: 'entryHeader',
+		date: new Date(2020, 0, 1),
+		activity: 'ACTIVITY'
+	});
+});
+it('parseHeader: bad date', () => {
+	expect(parseHeader('NOT A VALID DATE')).toEqual({
+		parseType: 'parseError',
+		error: 'invalid date'
+	});
+});
+it('parseHeader: missing activity', () => {
+	expect(parseHeader('2020-01-01')).toEqual({
+		parseType: 'parseError',
+		error: 'missing activity'
+	});
 });
