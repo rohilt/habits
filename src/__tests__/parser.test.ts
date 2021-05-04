f3353e769ec98fee0486ff2d87db7e8b4175ed7c
Rohil Tuli <rohil.tuli@gmail.com>
2021-05-04 18:12:27 -0400

    test: added in parseProperties tests

--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -1,4 +1,4 @@
-import { parseFileContents, parseHeader } from '../parser/parser';
+import { parseFileContents, parseHeader, parseProperties } from '../parser/parser';
 
 it('parseHeader: simple', () => {
 	expect(parseHeader('2020-01-01 ACTIVITY')).toEqual({
@@ -17,5 +17,28 @@ it('parseHeader: missing activity', () => {
 	expect(parseHeader('2020-01-01')).toEqual({
 		parseType: 'parseError',
 		error: 'missing activity'
+	});
+});
+
+it('parseProperties: simple', () => {
+	expect(parseProperties(['30 minutes', ':distance 1', ':location xyz'])).toEqual({
+		parseType: 'entryProperties',
+		time: 30,
+		distance: 1,
+		location: 'xyz'
+	});
+});
+
+it('parseProperties: missing time', () => {
+	expect(parseProperties([':distance 1', ':location xyz'])).toEqual({
+		parseType: 'parseError',
+		error: 'missing time property'
+	});
+});
+
+it('parseProperties: invalid property', () => {
+	expect(parseProperties([':distance 1', 'this is not valid', ':location xyz'])).toEqual({
+		parseType: 'parseError',
+		error: 'entry has a property which is neither arbitrary (:property) or time (min, hour)'
 	});
 });
