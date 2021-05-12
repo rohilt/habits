f2dffc6d2d1681089035be26aead988b46df1ce8
Rohil Tuli <rohil.tuli@gmail.com>
2021-05-12 14:58:03 -0400

    feat: added in doughnut chart instead of bar chart

--- src/components/Journal.svelte
+++ src/components/Journal.svelte
@@ -12,37 +12,26 @@
 	let ctx;
 	onMount(() => {
 		let chart = new Chart(ctx, {
-			type: 'bar',
+			type: 'doughnut',
 			data: {
-				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+				labels: ['Red', 'Blue', 'Yellow'],
 				datasets: [
 					{
-						label: '# of Votes',
-						data: [12, 19, 3, 5, 2, 3],
-						backgroundColor: [
-							'rgba(255, 99, 132, 0.2)',
-							'rgba(54, 162, 235, 0.2)',
-							'rgba(255, 206, 86, 0.2)',
-							'rgba(75, 192, 192, 0.2)',
-							'rgba(153, 102, 255, 0.2)',
-							'rgba(255, 159, 64, 0.2)'
-						],
-						borderColor: [
-							'rgba(255, 99, 132, 1)',
-							'rgba(54, 162, 235, 1)',
-							'rgba(255, 206, 86, 1)',
-							'rgba(75, 192, 192, 1)',
-							'rgba(153, 102, 255, 1)',
-							'rgba(255, 159, 64, 1)'
-						],
-						borderWidth: 1
+						label: 'My First Dataset',
+						data: [300, 50, 100],
+						backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+						hoverOffset: 4
 					}
 				]
 			},
 			options: {
-				scales: {
-					y: {
-						beginAtZero: true
+				responsive: true,
+				plugins: {
+					legend: {
+						position: 'bottom'
+					},
+					title: {
+						display: false
 					}
 				}
 			}
@@ -75,7 +64,7 @@
 		>
 	</div>
 	<div class="md:grid md:grid-cols-2 gap-16">
-		<div class="relative md:p-4">
+		<div class="relative md:p-8">
 			<canvas bind:this={ctx} id="myChart" />
 		</div>
 		<div class="flex flex-wrap gap-4 justify-items-center">
