878532b8cbfb9166e43ed07a39adcf3c71816b1d
Rohil Tuli <rohil.tuli@gmail.com>
2021-05-12 12:42:06 -0400

    feat: basic chart working!

--- src/components/Journal.svelte
+++ src/components/Journal.svelte
@@ -1,16 +1,59 @@
 <script lang="ts">
 	import type { Journal, ParseError } from '../parser/parser.types';
-	import { createEventDispatcher } from 'svelte';
+	import { createEventDispatcher, onMount } from 'svelte';
+	import Chart from 'chart.js/auto';
 
 	export let maybeJournal: Journal | ParseError;
 
 	const dispatch = createEventDispatcher();
+
+	let ctx;
+	$: console.log(ctx);
+	onMount(() => {
+		let chart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+				datasets: [
+					{
+						label: '# of Votes',
+						data: [12, 19, 3, 5, 2, 3],
+						backgroundColor: [
+							'rgba(255, 99, 132, 0.2)',
+							'rgba(54, 162, 235, 0.2)',
+							'rgba(255, 206, 86, 0.2)',
+							'rgba(75, 192, 192, 0.2)',
+							'rgba(153, 102, 255, 0.2)',
+							'rgba(255, 159, 64, 0.2)'
+						],
+						borderColor: [
+							'rgba(255, 99, 132, 1)',
+							'rgba(54, 162, 235, 1)',
+							'rgba(255, 206, 86, 1)',
+							'rgba(75, 192, 192, 1)',
+							'rgba(153, 102, 255, 1)',
+							'rgba(255, 159, 64, 1)'
+						],
+						borderWidth: 1
+					}
+				]
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
+			}
+		});
+	});
 </script>
 
 <div>
 	<button class="underline" on:click={() => dispatch('fileUpload', {})}
 		>(return to file upload)</button
 	>
+	<canvas bind:this={ctx} id="myChart" width="400" height="400" />
 	<div class="flex flex-wrap gap-4 justify-items-center">
 		{#if maybeJournal.parseType == 'journal'}
 			{#each maybeJournal.entries as journalEntry}
