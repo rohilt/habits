8891dd5a09551cfe1638b58647515fb2a0b6c047
Rohil Tuli <rohil.tuli@gmail.com>
2021-05-12 15:07:17 -0400

    feat: handled parse error properly
    nothing should be displayed except the parse error
    entire logic is wrapped around an if statement now

--- src/components/Journal.svelte
+++ src/components/Journal.svelte
@@ -8,67 +8,68 @@
 	const dispatch = createEventDispatcher();
 
 	let timeView = 'today';
-
 	let ctx;
 	onMount(() => {
-		let chart = new Chart(ctx, {
-			type: 'doughnut',
-			data: {
-				labels: ['Red', 'Blue', 'Yellow'],
-				datasets: [
-					{
-						label: 'My First Dataset',
-						data: [300, 50, 100],
-						backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
-						hoverOffset: 4
-					}
-				]
-			},
-			options: {
-				responsive: true,
-				plugins: {
-					legend: {
-						position: 'bottom'
-					},
-					title: {
-						display: false
+		let chart;
+		if (ctx)
+			chart = new Chart(ctx, {
+				type: 'doughnut',
+				data: {
+					labels: ['Red', 'Blue', 'Yellow'],
+					datasets: [
+						{
+							label: 'My First Dataset',
+							data: [300, 50, 100],
+							backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+							hoverOffset: 4
+						}
+					]
+				},
+				options: {
+					responsive: true,
+					plugins: {
+						legend: {
+							position: 'bottom'
+						},
+						title: {
+							display: false
+						}
 					}
 				}
-			}
-		});
+			});
 	});
 </script>
 
 <div class="flex flex-col items-center w-full items-stretch gap-4">
 	<button on:click={() => dispatch('fileUpload', {})}>(return to file upload)</button>
-	<div class="flex divide-x items-stretch md:w-1/2 md:self-center">
-		<button
-			class="flex-1 p-2 ring-gray-200"
-			class:underline={timeView == 'today'}
-			on:click={() => (timeView = 'today')}>today</button
-		>
-		<button
-			class="flex-1 p-2 ring-gray-200"
-			class:underline={timeView == 'this week'}
-			on:click={() => (timeView = 'this week')}>this week</button
-		>
-		<button
-			class="flex-1 p-2 ring-gray-200"
-			class:underline={timeView == 'this month'}
-			on:click={() => (timeView = 'this month')}>this month</button
-		>
-		<button
-			class="flex-1 p-2 ring-gray-200"
-			class:underline={timeView == 'all time'}
-			on:click={() => (timeView = 'all time')}>all time</button
-		>
-	</div>
-	<div class="md:grid md:grid-cols-2 gap-16">
-		<div class="relative md:p-8">
-			<canvas bind:this={ctx} id="myChart" />
+	{#if maybeJournal.parseType == 'journal'}
+		<div class="flex divide-x items-stretch md:w-1/2 md:self-center">
+			<button
+				class="flex-1 p-2 ring-gray-200"
+				class:underline={timeView == 'today'}
+				on:click={() => (timeView = 'today')}>today</button
+			>
+			<button
+				class="flex-1 p-2 ring-gray-200"
+				class:underline={timeView == 'this week'}
+				on:click={() => (timeView = 'this week')}>this week</button
+			>
+			<button
+				class="flex-1 p-2 ring-gray-200"
+				class:underline={timeView == 'this month'}
+				on:click={() => (timeView = 'this month')}>this month</button
+			>
+			<button
+				class="flex-1 p-2 ring-gray-200"
+				class:underline={timeView == 'all time'}
+				on:click={() => (timeView = 'all time')}>all time</button
+			>
 		</div>
-		<div class="flex flex-wrap gap-4 justify-items-center">
-			{#if maybeJournal.parseType == 'journal'}
+		<div class="md:grid md:grid-cols-2 gap-16">
+			<div class="relative md:p-8">
+				<canvas bind:this={ctx} id="myChart" />
+			</div>
+			<div class="flex flex-wrap gap-4 justify-items-center">
 				{#each maybeJournal.entries as journalEntry}
 					<div class="bg-gray-100 bg-opacity-50 rounded-xl p-8 ring ring-gray-100 shadow">
 						<div class="text-indigo-600 text-center font-italic">
@@ -83,11 +84,15 @@
 						</ul>
 					</div>
 				{/each}
-			{:else}
-				<p>parse error: {maybeJournal.error}</p>
-				<br />
-				<p>error at {maybeJournal.activity}, {maybeJournal.date}</p>
-			{/if}
+			</div>
 		</div>
-	</div>
+	{:else}
+		<div>
+			Oh no!
+			<br />
+			<p>parse error: {maybeJournal.error}</p>
+			<br />
+			<p>error at {maybeJournal.activity}, {maybeJournal.date}</p>
+		</div>
+	{/if}
 </div>
