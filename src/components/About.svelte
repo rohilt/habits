7d84a49bd018b6bf6c2d669845954436e37e727d
Rohil Tuli <rohil.tuli@gmail.com>
2021-05-12 18:16:57 -0400

    bug: fixed fade animation for about page

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,1 +1,5 @@
-<div class="px-8 md:px-16 py-8 in:fade">This is the about page.</div>
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+</script>
+
+<div class="px-8 md:px-16 py-8" in:fade>This is the about page.</div>
